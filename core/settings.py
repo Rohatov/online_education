28c3312f1728acf44fed42f7e4ec97f79c9a7611
Sherzod <105229066+Rohatov@users.noreply.github.com>
2025-03-26 16:22:30 +0500

    test deploy

--- core/settings.py
+++ core/settings.py
@@ -105,23 +105,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'online_education',  # Ma'lumotlar bazasining nomi
-        'USER': 'roxatov',  # PostgreSQL foydalanuvchi nomi
-        'PASSWORD': '933377448',  # PostgreSQL foydalanuvchi paroli
-        'HOST': 'localhost',  # PostgreSQL server manzili
-        'PORT': '5432',  # PostgreSQL porti (standart 5432)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
